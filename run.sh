1d9b5713f7d6c22126c706548a4bdcd6d760aca3
Caleb Lindgren <calebmlindgren@gmail.com>
2020-05-29 14:51:54 -0600

    Longer run

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-#SBATCH --time=07:00:00   # walltime
+#SBATCH --time=40:00:00   # walltime
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --mem-per-cpu=8192M   # memory per CPU core
+#SBATCH --mail-user=calebmlindgren@gmail.com   # email address
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
 
 jupyter nbconvert --ExecutePreprocessor.timeout=None --to notebook --inplace --execute pancancer_enrichment_step_1.ipynb
