32ed86d308ee7eb89787896173c20fd1759fb970
Caleb Lindgren <calebmlindgren@gmail.com>
2020-05-29 19:09:50 -0600

    Update logging

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH --time=40:00:00   # walltime
+#SBATCH --time=10:00:00   # walltime
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
-#SBATCH --mem-per-cpu=8192M   # memory per CPU core
+#SBATCH --mem-per-cpu=4096M   # memory per CPU core
 #SBATCH --mail-user=calebmlindgren@gmail.com   # email address
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
