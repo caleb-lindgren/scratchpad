a1c36db35e5bda5c7fc09aea2c8c08b3ea2cf59c
Caleb Lindgren <calebmlindgren@gmail.com>
2020-06-16 14:56:10 -0600

    Step 6 run

--- pancancer_enrichment/run_step6.sh
+++ pancancer_enrichment/run_step6.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-#SBATCH --time=01:00:00   # walltime
+#SBATCH --time=20:00:00   # walltime
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --mem-per-cpu=16384M   # memory per CPU core
 #SBATCH --mail-user=calebmlindgren@gmail.com   # email address
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
-#SBATCH --qos=test
 
 jupyter nbconvert --ExecutePreprocessor.timeout=None --to notebook --inplace --execute 6_find_enriched_pathways_gseapy_kegg.ipynb
