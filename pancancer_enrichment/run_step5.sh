1079ff59c5791eb442e248fff1d71bb448d181ad
Caleb Lindgren <calebmlindgren@gmail.com>
2020-06-17 07:15:02 -0600

    Successful step 5 run

--- pancancer_enrichment/run_step5.sh
+++ pancancer_enrichment/run_step5.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH --time=01:00:00   # walltime
+#SBATCH --time=02:00:00   # walltime
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
-#SBATCH --mem-per-cpu=65536M   # memory per CPU core
+#SBATCH --mem-per-cpu=262144M   # memory per CPU core
 #SBATCH --mail-user=calebmlindgren@gmail.com   # email address
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
