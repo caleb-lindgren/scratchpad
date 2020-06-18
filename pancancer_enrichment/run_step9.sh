4201c60404322a0082c22508d51452c5f50938f7
Caleb Lindgren <calebmlindgren@gmail.com>
2020-06-18 12:43:37 -0600

    Create step 9 run script

---
+++ pancancer_enrichment/run_step9.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --time=10:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --mem-per-cpu=16384M   # memory per CPU core
+#SBATCH --mail-user=calebmlindgren@gmail.com   # email address
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+jupyter nbconvert --ExecutePreprocessor.timeout=None --to notebook --inplace --execute 9_gseapy_prerank.ipynb
