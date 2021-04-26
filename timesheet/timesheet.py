9151aa11d79622471e7abad0d667b1ee03f44fb4
Caleb Lindgren <calebmlindgren@gmail.com>
2021-04-26 08:53:43 -0600

    Work on timesheet

--- timesheet/timesheet.py
+++ timesheet/timesheet.py
@@ -9,7 +9,7 @@ class Timesheet:
         self._timesheet_path = os.path.abspath(timesheet_path)
 
         if os.path.isfile(self._timesheet_path):
-            self.timesheet = pd.read_csv(self._timesheet_path, sep="\t")
+            self.timesheet = pd.read_csv(self._timesheet_path, sep="\t", parse_dates=["time"])
         else:
             print(f"Existing timesheet not found. Creating new timesheet at '{self._timesheet_path}'.")
             self.timesheet = pd.DataFrame()
@@ -22,40 +22,43 @@ class Timesheet:
 
     def punch(self, io, time=pd.Timestamp.now()):
 
-        self.timesheet.append({
+        tmp = self.timesheet.append({
             "time": time,
             "io": io
-        })
+        },
+        ignore_index=True)
 
-        print(self.summarize_week)
+        if tmp.duplicated().any():
+            raise ValueError(f"Invalid punch. Creates duplicate row.")
+
+        self.timesheet = tmp.sort_values(by="time").reset_index(drop=True)
+
+        print(self.summarize_week())
 
     def save(self):
-        self.timesheet.to_csv(self._timesheet_path, sep="\t"), index=False)
+        self.timesheet.to_csv(self._timesheet_path, sep="\t", index=False)
         print(f"Timesheet saved to '{self._timesheet_path}'.")
 
 
-if len(sys.argv < 2):
+if len(sys.argv) < 2:
     raise ValueError("Insufficient number of arguments passed. Please specify 'in' or 'out'.")
 
-if len(sys.argv == 2):
+if len(sys.argv) in (2, 7):
 
     if sys.argv[1] in ("in", "out"):
         ts = Timesheet()
-        ts.punch(io=sys.argv[1])
-        ts.save()
 
-    else:
-        raise ValueError(f"Invalid punch type. You passed '{sys.argv[1]}'. Please pass 'in' or 'out'.")
+        if len(sys.argv) == 2:
+            ts.punch(io=sys.argv[1])
+        else:
+            punch_time_list = [int(i) for i in sys.argv[2:]]
+            time = pd.Timestamp(*punch_time_list)
+            ts.punch(io=sys.argv[1], time=time)
 
-elif len(sys.argv == 7):
-
-    if sys.argv[1] in ("in", "out"):
-        ts = Timesheet()
-        ts.punch(io=sys.argv[1])
         ts.save()
 
     else:
         raise ValueError(f"Invalid punch type. You passed '{sys.argv[1]}'. Please pass 'in' or 'out'.")
 
 else:
-    raise ValueError(f"Too many arguments. You passed {len(sys.argv) - 1} arguments. Pass either one ('in' or 'out') or six ('in' or 'out' and year, month, day, 24 hour, minute).")
+    raise ValueError(f"Wrong number of arguments. You passed {len(sys.argv) - 1} arguments. Pass either one ('in' or 'out') or six ('in' or 'out' and year, month, day, 24 hour, minute).")
