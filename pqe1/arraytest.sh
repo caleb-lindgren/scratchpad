b56dee571f1575be665b66fc84e14568b5272961
Caleb Lindgren <calebmlindgren@gmail.com>
2024-06-04 02:22:53 -0400

    Testing

---
+++ pqe1/arraytest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH -c 1
+#SBATCH -t 0-01:00
+#SBATCH -p short
+#SBATCH --mem=2G
+#SBATCH -o log/slurm_%j_%a.out 
+#SBATCH -e log/slurm_%j_%a.err 
+#SBATCH --array=0-999
+
+python simulate.py $SLURM_ARRAY_TASK_ID
