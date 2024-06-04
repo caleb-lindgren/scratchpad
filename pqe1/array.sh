4258e3d2a58b0d26566caa9e19ade63f791b6d43
Caleb Lindgren <calebmlindgren@gmail.com>
2024-06-04 11:02:18 -0400

    Restructure

--- pqe1/array.sh
+++ pqe1/array.sh
@@ -3,9 +3,10 @@
 #SBATCH -c 1
 #SBATCH -t 0-00:10
 #SBATCH -p short
-#SBATCH --mem=100M
+#SBATCH --mem=1000M
 #SBATCH -o log/slurm_%j_%a.out 
 #SBATCH -e log/slurm_%j_%a.err 
 #SBATCH --array=0-9999
 
-python simulate.py $((SLURM_ARRAY_TASK_ID + 220000))
+echo $((SIM_MULT * 10000 + SLURM_ARRAY_TASK_ID ))
+python simulate.py $((SIM_MULT * 10000 + SLURM_ARRAY_TASK_ID ))
