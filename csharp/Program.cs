887586858b2258f5f0660282b3cf759db274f73f
Caleb Lindgren <calebmlindgren@gmail.com>
2024-11-30 16:12:33 -0500

    Update scratch

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,12 +1,24 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
-static (double, double) GetPPMRange(double mz, double ppm)
+List<(int rt, string target)> vals = new List<(int, string)>();
+
+vals.Add((1, "D"));
+vals.Add((2, "C"));
+vals.Add((1, "B"));
+vals.Add((3, "A"));
+
+for (int i = 0; i < vals.Count; i++)
 {
-	double min = mz * (1 - ppm / 1e6);
-	double max = mz * (1 + ppm / 1e6);
-
-	return (min, max);
+    Console.WriteLine($"{vals[i].rt} - {vals[i].target}");
 }
 
-Console.WriteLine(GetPPMRange(600, 5));
-Console.WriteLine(GetPPMRange(1638, 3.14159265358979323));
+Console.WriteLine();
+
+vals.Sort();
+
+for (int i = 0; i < vals.Count; i++)
+{
+    Console.WriteLine($"{vals[i].rt} - {vals[i].target}");
+}
