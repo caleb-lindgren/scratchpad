811d1fce7de833498820402c8f2300e725e1dc45
Caleb Lindgren <calebmlindgren@gmail.com>
2025-02-10 17:14:47 -0500

    Switch to dotnet 9

---
+++ dotnet9/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    	int l = Int32.Parse(args[0]);
+    	int d = Int32.Parse(args[1]);
+
+    	Console.WriteLine(l * d);
+    }
+}
